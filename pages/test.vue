235d0c3ce63c827d498d9f8a8b9c31e0941f2349
huaiyugong <huaiyugong123@gmail.com>
2020-01-20 22:10:21 +0800

    test: add test

---
+++ pages/test.vue
@@ -0,0 +1,25 @@
+<template>
+  <div></div>
+</template>
+<script>
+import { testPostApi } from "../service/apis";
+import { testGetApi } from "../service/apis";
+import { testDeleteApi } from "../service/apis";
+export default {
+  created() {
+    this.testApi();
+  },
+  methods: {
+    testApi() {
+      let formData = { key: "value" };
+      testPostApi(formData)
+        .then(chunck => {
+          console.dir(chunck);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }
+};
+</script>
