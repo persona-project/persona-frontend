5acd77938d601037a8efdef337d95ae5f77df264
huaiyugong <huaiyugong123@gmail.com>
2020-01-30 22:09:27 +0800

    feat :finish the  structur of crowd page

--- pages/datacenter/crowd/index.vue
+++ pages/datacenter/crowd/index.vue
@@ -1,107 +1,130 @@
 <template>
   <div id="box">
-    <div class="box" v-if="flag" infinite-scroll-disabled="disabled" >
-        <ul class="list" >
-          <li>
-           <line-chart v-bind:recordData="recordData" v-if="flag"></line-chart>
-          </li>
-          <li>
-           <line-chart v-bind:recordData="recordData" v-if="flag"></line-chart>
-          </li>
-          <li>
-           <line-chart v-bind:recordData="recordData" v-if="flag"></line-chart>
-          </li>
-          <!--li v-for="(i,index) in list" class="list-item" :key="index">{{ i.noticeTitle }}</li-->
+    <el-scrollbar style="height:100%">
+      <div class="box"  infinite-scroll-disabled="disabled">
+        <ul class="list container">
+          <el-card shadow="hover" class="chart-card">
+            <h3>MOOC用户年龄比例</h3>
+            <el-divider></el-divider>
+            <pie-chart v-bind:recordData="ageData" v-if="ageFlag"></pie-chart>
+          </el-card>
+          <el-card shadow="hover" class="chart-card">
+            <h3>MOOC用户地域比例</h3>
+            <el-divider></el-divider>
+            <ch-map v-bind:recordData="areData" v-if="areaFlag"></ch-map>
+          </el-card>
+          <el-card shadow="hover" class="chart-card">
+            <h3>MOOC用户性别比</h3>
+            <el-divider></el-divider>
+            <pie-chart v-bind:recordData="sexData" v-if="sexFlag"></pie-chart>
+          </el-card>
         </ul>
-        <!--p v-if="loading" style="margin-top:10px;" class="loading">
-          <span></span>
-        </p>
-        <p v-if="noMore" style="margin-top:10px;font-size:13px;color:#ccc">没有更多了</p-->
-    </div>
+      </div>
+    </el-scrollbar>
   </div>
 </template>
 
 <script>
-import {getAgeCnt} from "../../../service/apis"
-import {list2Ob} from "../../../utils/dispose"
+import { getAgeCnt } from "../../../service/apis";
+import { list2Ob } from "../../../utils/dispose";
+import { getAreaCnt } from "../../../service/apis";
+import { getSexCnt } from "../../../service/apis";
 export default {
   data() {
     return {
-      flag:false,
-       recordData:{
-        title:"test////dialogue",
-        xAxis:"xAxis",
-        yAxis:"yAxis",
+      ageFlag: false,
+      areaFlag: false,
+      sexFlag: false,
+      ageData: {
+        title: "test////dialogue",
+        xAxis: "xAxis",
+        yAxis: "yAxis",
+        data: [],
+        name: "年龄占比"
+      },
+      areData: {
+        titile: "test-map",
+        xAxis: "xAxis",
+        yAxis: "yAxis",
+        data: [
+          { name: "上海", value: 100 },
+          { name: "重庆", value: 2 }
+        ],
+        name: "地区占比"
+      },
+      sexData:{
         data:[]
       }
     };
   },
-  computed: {
-    noMore() {
-      //当起始页数大于总页数时停止加载
-      return this.count >= this.totalPages - 1;
-    },
-    disabled() {
-      return this.loading || this.noMore;
-    }
-  },
   created() {
     this.getMessage();
+    this.getAreaMessage();
+    this.getSexMessage();
   },
   methods: {
-   getMessage(){
-     let params={}
-     getAgeCnt(params).then(chunck=>{
-       console.dir(chunck)
-       let data=list2Ob(chunck)
-       this.recordData.data=data.count
-       console.log(data.count)
-       this.flag=true
-     }).catch(err=>{
-       console.log(err)
-     })
-   }
+    getAreaMessage() {
+      let params = {};
+      getAreaCnt(params).then(chunck => {
+        this.areData.data = chunck.payload;
+        console.log(`++++++++++++++++mapdata is ${this.areaData.data}`);
+        this.areaFlag = true;
+      });
+    },
+    getSexMessage(){
+      let params={};
+      getSexCnt(params).then(chunck=>{
+        this.sexData=chunck.payload
+        this.sexFalg=true
+      })
+    
+    },
+    getMessage() {
+      let params = {};
+      getAgeCnt(params)
+        .then(chunck => {
+          console.dir(chunck);
+          let data = list2Ob(chunck);
+          let test = [];
+          chunck.forEach(element => {
+            test.push([`${element.type} 岁`, element.count]);
+          });
+          this.ageData.data = test;
+          console.log(data.count);
+          this.ageFlag = true;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
   }
 };
 </script>
-
+<style lang="scss">
+.el-scrollbar__wrap {
+  overflow-x: hidden;
+}
+</style>
 <style scoped>
-#box{
-    width: 80%;
+#box {
+  width: 80%;
   height: 88%;
   position: absolute;
   overflow-y: auto;
 }
 .box {
   width: 100%;
-   margin:  0 auto;
+  margin: 0 auto;
 }
 .list {
   padding: 0;
   font-size: 14px;
 }
-/* .list-item {
-  width: 100%;
-  display: block;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  list-style: none;
-  padding: 0 1rem;
-  box-sizing: border-box;
-  height: 70px;
-  line-height: 70px;
-  border-bottom: 1px solid #e7e7e7;
-} */
-/* .loading span {
-  display: inline-block;
-  width: 20px;
-  height: 20px;
-  border: 2px solid #409eff;
-  border-left: transparent;
-  animation: zhuan 0.5s linear infinite;
-  border-radius: 50%;
-} */
+.chart-card {
+  width: 48%;
+  margin: 1%;
+}
+
 @keyframes zhuan {
   0% {
     transform: rotate(0);
@@ -109,5 +132,12 @@ export default {
   100% {
     transform: rotate(360deg);
   }
+}
+</style>
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
 }
 </style>
